15745198b85e27779b2b0b993ff452cddfabb5d0
liuyaohui <klyhb@qq.com>
2024-10-17 11:46:17 +0800

    1236项目最终版固化代码

---
+++ 1236/Debug/src/mem_test.d
@@ -0,0 +1,15 @@
+src/mem_test.o src/mem_test.o: ../src/mem_test.c \
+ ../../1236_bsp/microblaze_1/include/xil_printf.h \
+ ../../1236_bsp/microblaze_1/include/xil_types.h \
+ ../../1236_bsp/microblaze_1/include/xparameters.h \
+ ../../1236_bsp/microblaze_1/include/bspconfig.h ../src/mem_test.h
+
+../../1236_bsp/microblaze_1/include/xil_printf.h:
+
+../../1236_bsp/microblaze_1/include/xil_types.h:
+
+../../1236_bsp/microblaze_1/include/xparameters.h:
+
+../../1236_bsp/microblaze_1/include/bspconfig.h:
+
+../src/mem_test.h:
